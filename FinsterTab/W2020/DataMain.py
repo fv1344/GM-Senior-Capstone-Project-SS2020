93d419cf8776d1b99d2200995a9c6a8cd414b3b0
William Aman <gr8377@wayne.edu>
2020-06-28 16:00:57 -0400

    Added if statement instead of comment for the population of the date dimension table

--- FinsterTab/W2020/DataMain.py
+++ FinsterTab/W2020/DataMain.py
@@ -7,33 +7,38 @@ from FinsterTab.W2020.EngineeredFeatures import EngineeredFeatures
 from FinsterTab.W2020.TradingSimulator import TradingSimulator
 import FinsterTab.W2020.AccuracyTest
 
-
-
-# create database connection
-db_engine = DBEngine().mysql_engine()
-
-
-
+""" 
+    Variables
+"""
 # instrument symbol table
 instrument_master = 'dbo_instrumentmaster'
+# Change to true if date dimension table is empty
+reset_date_dim = False
 
-# Get Raw Market Data
+
+"""
+    Operations
+"""
+
+# Create database connection
+db_engine = DBEngine().mysql_engine()
+
+# Get raw market data
 master_data = DataFetch(db_engine, instrument_master)
 
-# get ticker symbols
+# Get ticker symbols
 ticker_symbols = master_data.get_datasources()
 
-# get data from Yahoo! Finance and store in InstrumentStatistics
+# Get data from Yahoo! Finance and store in InstrumentStatistics
 master_data.get_data(ticker_symbols)
 
 
-# get date data and store in DateDim, replaced the SQL calendar code
-# only run this if date dim table is empty
-# master_data.get_calendar()
+# Get date data and store in DateDim, replaced the SQL calendar code
+if reset_date_dim:
+    master_data.get_calendar()
 
 
-
-#Macro Economic Variable Functions
+# Calculate forecast with functions that use macroeconomic indicators
 FinsterTab.W2020.AccuracyTest.get_past_data(db_engine)
 DataFetch.macroFetch(db_engine)
 DataForecast.MSF1(db_engine)
@@ -41,33 +46,41 @@ DataForecast.MSF2(db_engine)
 DataForecast.MSF3(db_engine)
 DataForecast.MSF2_Past_Date(db_engine)
 
-
-
-# calculate technical indicators and store in EngineeredFeatures
+# Get raw data from database to calculate forecasts
 indicators = EngineeredFeatures(db_engine, instrument_master)
+
+# Calculate technical indicators and store in EngineeredFeatures
 indicators.calculate()
 
-# Get Raw Data from database to calculate forecasts
+
+# Get raw data from database to calculate forecasts
 forecast = DataForecast(db_engine, instrument_master)
 
-# Polynomial Regression Function
+
+# Polynomial regression function
 # Takes a while to run, comment out if need be
 forecast.calculate_regression()
+
 
 # calculate and store price predictions
 forecast.calculate_forecast()
 
+
 # calculate and store ARIMA forecast
 forecast.calculate_arima_forecast()
+
 
 # calculate and store Random Forest forecast
 forecast.calculate_random_forest_forecast()
 
+
 # flawed price prediction from previous semesters, without our improvements
 forecast.calculate_forecast_old()
 
+
 # calculate and store SVM forecast
 forecast.calculate_svm_forecast()
+
 
 # calculate and store XGBoost forecast
 forecast.calculate_xgboost_forecast()
@@ -76,32 +89,38 @@ forecast.calculate_xgboost_forecast()
 # Get Raw Data and Technical Indicators
 signals = BuySell(db_engine, instrument_master)
 
+
 # generate CMA trade signals
 signals.cma_signal()
+
 
 # generate FRL trade signals
 signals.frl_signal()
 
+
 # generate EMA trade signals
 signals.ema_signal()
+
 
 # generate MACD signals
 signals.macd_signal()
 
+
 # forecast-based signals
 signals.algo_signal()
+
 
 # Run Trade Simulations Based on Trade Signals
 simulator = TradingSimulator(db_engine, instrument_master)
 
+
 # individual strategies
 simulator.trade_sim()
+
 
 # combination trading strategy
 simulator.combination_trade_sim()
 
+
 # buy and hold simulation
 simulator.buy_hold_sim()
-
-
-
