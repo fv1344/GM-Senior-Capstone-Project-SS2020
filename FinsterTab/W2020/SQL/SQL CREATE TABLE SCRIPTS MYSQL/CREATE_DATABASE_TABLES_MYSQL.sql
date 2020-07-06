f43ceebe21f1b67fa4015896051fdbf8736c9f03
Padmini Jayanti <58052271+padmini27@users.noreply.github.com>
2020-07-06 18:41:58 -0400

    Complete schema with relationships

--- FinsterTab/W2020/SQL/SQL CREATE TABLE SCRIPTS MYSQL/CREATE_DATABASE_TABLES_MYSQL.sql
+++ FinsterTab/W2020/SQL/SQL CREATE TABLE SCRIPTS MYSQL/CREATE_DATABASE_TABLES_MYSQL.sql
@@ -8,77 +8,6 @@
 
 use GMFSP_db;
 
-DROP TABLE IF EXISTS dbo_strategymaster;
-create table dbo_strategymaster 
-(          
-strategycode        varchar(20),
-strategyname        varchar(50),
-primary key (strategycode) 
-)
-;
-
-
-DROP TABLE IF EXISTS dbo_algorithmforecast;
-create table dbo_algorithmforecast 
-(          
-forecastdate            date,
-instrumentid            int,
-forecastcloseprice      float,
-algorithmcode           varchar(50),
-prederror               float,
-primary key (forecastdate, instrumentid, algorithmcode)
-)
-;
-
-
-DROP TABLE IF EXISTS dbo_actionsignals;
-create table dbo_actionsignals 
-(          
-date                  date,
-instrumentid          int,
-strategycode          varchar(50),
-`signal`              int,
-primary key (date, instrumentid, strategycode)
-)
-;
-
-
-DROP TABLE IF EXISTS dbo_algorithmmaster;
-create table dbo_algorithmmaster 
-(          
-algorithmcode         varchar(50),
-algorithmname         varchar(50),
-primary key (algorithmcode)
-)
-;
-
-
-drop table if exists dbo_instrumentmaster;  
-create table dbo_instrumentmaster(
-instrumentid            int,
-instrumentname          varchar(50),
-`type`                  varchar(50),
-exchangename            varchar(50),
-primary key (instrumentid)
-)
-;
-
-
-DROP TABLE IF EXISTS dbo_instrumentstatistics;
-create table dbo_instrumentstatistics(        
-date                    date,   
-high                    float,
-low                     float,
-`open`                  float,
-`close`                 float,
-volume                  float,
-`adj close`             float,
-instrumentid            int,
-primary key (date, instrumentid)
-)
-;
-
-
 DROP TABLE IF EXISTS dbo_datedim;
 create table dbo_datedim(
 `date`                 date,
@@ -91,17 +20,143 @@ primary key (date)
 )
 ;
 
+drop table if exists dbo_instrumentmaster;  
+create table dbo_instrumentmaster(
+instrumentid            int,
+instrumentname          varchar(50),
+`type`                  varchar(50),
+exchangename            varchar(50),
+primary key (instrumentid)
+)
+;
+
+
+/* MUST INSERT VALUES INTO INSTRUMENTMASTER TABLE IN ORDER FOR DATABASE TO WORK */
+/* USE INSERT_INTO_INSTRUMENT_MASTER_MYSQL.sql FILE TO ADD SYMBOLS              */
+/* USE REMOVE_FROM_INSTRUMENT_MASTER_MYSQL.sql FILE TO REMOVE SYMBOLS           */
+
+insert into dbo_instrumentmaster
+values (1 , 'GM'   , 'Equity' , 'YAHOO'),
+	   (2 , 'PFE'  , 'Equity' , 'YAHOO'),
+	   (3 , 'SPY'  , 'Equity' , 'YAHOO'),
+	   (4 , 'XPH'  , 'Equity' , 'YAHOO'),
+	   (5 , 'CARZ' , 'Equity' , 'YAHOO'),
+       (6 , '^TYX' , 'Equity' , 'YAHOO'),
+	(7, 'FCAU' , 'Equity' , 'YAHOO'),
+	(8, 'TM' , 'Equity', 'YAHOO'),
+	(9, 'F', 'Equity' , 'YAHOO'),
+	(10, 'HMC' , 'Equity' , 'YAHOO')
+;
+
+
+drop table if exists dbo_macroeconmaster;  
+create table dbo_macroeconmaster(
+macroeconcode          varchar(10),
+macroeconname          varchar(50),
+accesssourcekey        varchar(50),
+accesssource		varchar(50),
+datecreated		 date,
+activecode		varchar(10),
+primary key (macroeconname)
+);
+
+insert into dbo_macroeconmaster
+values ('GDP' , 'GDP'   , 'FRED/NGDPPOT', 'Quandl', 0, 'A'),
+	   ('UR' , 'Unemployment Rate'  , 'USMISERY/INDEX', 'Quandl', 0, 'A'),
+	   ('IR' , 'Inflation Rate'  , 'USMISERY/INDEX', 'Quandl', 0, 'A'),
+	   ('MI' , 'Misery Index'  , 'USMISERY/INDEX', 'Quandl', 0, 'A'),
+       ('TYX', '30 Year Bond Yield', '^TYX', 'Yahoo', 0, 'I'),
+       ('COVI', 'Crude Oil ETF Volatility Index', 'OVXCLS', 'FRED', 0, 'A'),
+       ('CPIUC', 'Consumer Price Index Urban Consumers', 'CPIAUCSL', 'FRED', 0, 'A'),
+       ('FSI', 'Financial Stress Index', 'STLFSI', 'FRED', 0, 'A')
+;
+
+DROP TABLE IF EXISTS dbo_instrumentstatistics;
+create table dbo_instrumentstatistics(        
+date                    date,   
+high                    float,
+low                     float,
+`open`                  float,
+`close`                 float,
+volume                  float,
+`adj close`             float,
+instrumentid            int,
+foreign key (date) references dbo_datedim(date),
+foreign key (instrumentid) references dbo_instrumentmaster(instrumentid)
+)
+;
+
+
+DROP TABLE IF EXISTS dbo_macroeconstatistics;
+CREATE TABLE dbo_macroeconstatistics (
+	date	date,
+	statistics	int,
+	macroeconname          varchar(50),
+   	macroeconcode	varchar(10),
+	foreign key (macroeconname) references dbo_macroeconmaster(macroeconname)
+    );
+
+
+DROP TABLE IF EXISTS dbo_strategymaster;
+create table dbo_strategymaster 
+(          
+strategycode        varchar(20),
+strategyname        varchar(50),
+primary key (strategycode) 
+)
+;
+
+DROP TABLE IF EXISTS dbo_algorithmmaster;
+create table dbo_algorithmmaster 
+(          
+algorithmcode         varchar(50),
+algorithmname         varchar(50),
+primary key (algorithmcode)
+)
+;
+
+
+DROP TABLE IF EXISTS dbo_algorithmforecast;
+create table dbo_algorithmforecast 
+(          
+forecastdate            date,
+instrumentid            int,
+forecastcloseprice      float,
+algorithmcode           varchar(50),
+prederror               float,
+foreign key (forecastdate) references dbo_datedim(date),
+foreign key (instrumentid) references dbo_instrumentmaster(instrumentid),
+foreign key (algorithmcode) references dbo_algorithmmaster(algorithmcode)
+)
+;
+
+
+DROP TABLE IF EXISTS dbo_actionsignals;
+create table dbo_actionsignals 
+(          
+date                  date,
+instrumentid          int,
+strategycode          varchar(20),
+`signal`              int,
+foreign key (date) references dbo_datedim(date),
+foreign key (instrumentid) references dbo_instrumentmaster(instrumentid),
+foreign key (strategycode) references dbo_strategymaster(strategycode)
+)
+;
+
 
 DROP TABLE IF EXISTS dbo_statisticalreturns;
 create table dbo_statisticalreturns
 (           
 `date`                 date,
 instrumentid           int,
-strategycode           varchar(50),
+strategycode           varchar(20),
 positionsize           int,
 cashonhand             float,
 portfoliovalue         float,
-primary key (date, instrumentid, strategycode)
+foreign key (date) references dbo_datedim(date),
+foreign key (instrumentid) references dbo_instrumentmaster(instrumentid),
+foreign key (strategycode) references dbo_strategymaster(strategycode)
 )
 ;
 
@@ -133,69 +188,25 @@ sema                         float,
 mema                         float,
 lema                         float,
 volume_delta                 float,
-primary key (date, instrumentid)
+foreign key (date) references dbo_datedim(date),
+foreign key (instrumentid) references dbo_instrumentmaster(instrumentid)
 )
 ;
 
 
-
-/* MUST INSERT VALUES INTO INSTRUMENTMASTER TABLE IN ORDER FOR DATABASE TO WORK */
-/* USE INSERT_INTO_INSTRUMENT_MASTER_MYSQL.sql FILE TO ADD SYMBOLS              */
-/* USE REMOVE_FROM_INSTRUMENT_MASTER_MYSQL.sql FILE TO REMOVE SYMBOLS           */
-
-TRUNCATE TABLE dbo_instrumentmaster;
-insert into dbo_instrumentmaster
-values (1 , 'GM'   , 'Equity' , 'YAHOO'),
-	   (2 , 'PFE'  , 'Equity' , 'YAHOO'),
-	   (3 , 'SPY'  , 'Equity' , 'YAHOO'),
-	   (4 , 'XPH'  , 'Equity' , 'YAHOO'),
-	   (5 , 'CARZ' , 'Equity' , 'YAHOO'),
-       (6 , '^TYX' , 'Equity' , 'YAHOO'),
-	(7, 'FCAU' , 'Equity' , 'YAHOO'),
-	(8, 'TM' , 'Equity', 'YAHOO'),
-	(9, 'F', 'Equity' , 'YAHOO'),
-	(10, 'HMC' , 'Equity' , 'YAHOO')
-;
-
-drop table if exists dbo_macroeconmaster;  
-create table dbo_macroeconmaster(
-macroeconcode          varchar(10),
-macroeconname          varchar(50),
-accesssourcekey        varchar(50),
-accesssource		varchar(50),
-datecreated		 date,
-activecode		varchar(10),
-primary key (macroeconname)
-);
-
-insert into dbo_macroeconmaster
-values ('GDP' , 'GDP'   , 'FRED/NGDPPOT', 'Quandl', 0, 'A'),
-	   ('UR' , 'Unemployment Rate'  , 'USMISERY/INDEX', 'Quandl', 0, 'A'),
-	   ('IR' , 'Inflation Rate'  , 'USMISERY/INDEX', 'Quandl', 0, 'A'),
-	   ('MI' , 'Misery Index'  , 'USMISERY/INDEX', 'Quandl', 0, 'A'),
-       ('TYX', '30 Year Bond Yield', '^TYX', 'Yahoo', 0, 'I'),
-       ('COVI', 'Crude Oil ETF Volatility Index', 'OVXCLS', 'FRED', 0, 'A'),
-       ('CPIUC', 'Consumer Price Index Urban Consumers', 'CPIAUCSL', 'FRED', 0, 'A'),
-       ('FSI', 'Financial Stress Index', 'STLFSI', 'FRED', 0, 'A')
-;
-
-DROP TABLE IF EXISTS dbo_macroeconstatistics;
-CREATE TABLE dbo_macroeconstatistics (
-	date	date,
-	statistics	int,
-    macroeconcode	varchar(10),
-	primary key (macroeconcode)
-    );
 
 DROP TABLE IF EXISTS dbo_macroeconalgorithmforecast;
 CREATE TABLE dbo_macroeconalgorithmforecast(
 forecastdate            date,
 instrumentid            int,
 macroeconcode		varchar(10),
+macroeconname          varchar(50),
 forecastprice           float,
 algorithmcode           varchar(50),
 prederror               float,
-primary key (instrumentid)
+foreign key (forecastdate) references dbo_datedim(date),
+foreign key (instrumentid) references dbo_instrumentmaster(instrumentid),
+foreign key (macroeconname) references dbo_macroeconmaster(macroeconname)
 );
 
 DROP TABLE IF EXISTS dbo_tempvisualize;
@@ -203,7 +214,9 @@ CREATE TABLE dbo_tempvisualize(
 	forecastdate	date,
     instrumentid	int,
     forecastcloseprice	float,
-    algorithmcode	varchar(50)
+    algorithmcode	varchar(50),
+	foreign key (forecastdate) references dbo_datedim(date),
+	foreign key (instrumentid) references dbo_instrumentmaster(instrumentid)
 );
 
 DROP TABLE IF EXISTS dbo_paststatistics;
@@ -215,5 +228,6 @@ low                     float,
 `close`                 float,
 volume                  float,
 `adj close`             float,
-instrumentid            int
+instrumentid            int,
+foreign key (date) references dbo_datedim(date)
 )
